3e360f982523a7a8cca56c922ce2b65584e6a252
OumaimaSf <oumaimasaaouf@gmail.com>
2023-04-22 02:31:55 +0000

    all tasks done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 
-
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter = 'a';
+    char letter = 'a';
 
-	while (letter <= 'z')
-	{
-	    putchar(letter);
-	    letter++;
-	}
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
 
-	putchar('\n');
-	return 0;
+    putchar('\n');
+    return (0);
 }
