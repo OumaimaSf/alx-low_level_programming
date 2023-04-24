e37a3c5cf8a24b1dfdf6b4bd06ca1f6667a1964d
OumaimaSf <oumaimasaaouf@gmail.com>
2023-04-24 20:43:31 +0100

    4 Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzlle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
