f4317dca03defe02f85578669f64701aefa4a156
OumaimaSf <oumaimasaaouf@gmail.com>
2023-04-26 21:29:56 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ * add - adds two integers
+ * @a: the first integer to be added
+ * @b: the second integer to be added
+ *
+ * Return: the sum of the two integers
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	int digit, sign = 1;
+
+	if (sum < 0)
+	{
+		sign = -1;
+		sum *= -1;
+		_putchar('-');
+	}
+	
+	if (sum / 10)
+		add(sum / 10, 0);
+
+	digit = (sum % 10) + '0';
+	_putchar(digit);
+
+	return (sign * sum);
+}
