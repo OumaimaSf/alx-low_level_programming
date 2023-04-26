384a2d24ddbcba0cc27d4cb7dfc7fadaecef521b
OumaimaSf <oumaimasaaouf@gmail.com>
2023-04-26 21:05:50 +0100

    times_table

---
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -0,0 +1,36 @@
+#include"main.h"
+
+/**
+ * times_table - print the 9 times table, starting with 0
+ */
+
+void times_tables(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
+		{
+			product = i * j;
+
+			if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
