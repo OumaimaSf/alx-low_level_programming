2950ec0e95198ff44baccd18db87189f802c1978
OumaimaSf <oumaimasaaouf@gmail.com>
2023-04-27 00:28:44 +0100

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,35 +6,12 @@
  *
  */
 
-void print_to_98(int n)
+int main(void)
 {
-	int i;
-
-	if (n <= 98)
-	{
-		for (i = n; i < 98; i++)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	else
-	{
-		for (i = n; i > 97; i--)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
 }
