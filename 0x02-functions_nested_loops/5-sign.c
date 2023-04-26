a7377f4f35d55a566474cf22535cebb2c1f33c83
OumaimaSf <oumaimasaaouf@gmail.com>
2023-04-26 19:15:56 +0100

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n is positive, 0 if it is zero, -1 if it is negative
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
