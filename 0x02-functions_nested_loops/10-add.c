94cbf323a67d4681108fb18fd9bc338456bf152a
OumaimaSf <oumaimasaaouf@gmail.com>
2023-04-26 21:43:40 +0100

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,21 +9,5 @@
 
 int add(int a, int b)
 {
-	int sum = a + b;
-	int digit, sign = 1;
-
-	if (sum < 0)
-	{
-		sign = -1;
-		sum *= -1;
-		_putchar('-');
-	}
-	
-	if (sum / 10)
-		add(sum / 10, 0);
-
-	digit = (sum % 10) + '0';
-	_putchar(digit);
-
 	return (a + b);
 }
