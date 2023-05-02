288e9d67773235414567baf729b9a37d4a279f8e
OumaimaSf <oumaimasaaouf@gmail.com>
2023-05-03 00:53:05 +0100

    Do not swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,9 @@
 #include "main.h"
-
-/*
- * swap_int - fontion swaps the values of two integers
- * 
- * @a: Pointer to the first integer to swap
- * @b: Pointer to the second integer to swap
+/**
+ * swap_int - swap the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
-
 void swap_int(int *a, int *b)
 {
 	int tmp;
