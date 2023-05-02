5e98141b968c165b3cddc896cdfd50f83d55d852
OumaimaSf <oumaimasaaouf@gmail.com>
2023-05-03 00:48:47 +0100

    Do not swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,15 +5,13 @@
  * 
  * @a: Pointer to the first integer to swap
  * @b: Pointer to the second integer to swap
- *
- * Description: This function swaps the values of two integers using pointers
- *
- * Return: void
  */
 
 void swap_int(int *a, int *b)
 {
-	int tmp = *a;
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
