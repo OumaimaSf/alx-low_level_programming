bbb25b75f82f7fd77977b2231936ce62a92ec019
OumaimaSf <oumaimasaaouf@gmail.com>
2023-05-03 20:49:29 +0100

    keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,4 +8,23 @@
  */
 int main (void)
 {
+	char password[100];
+	int i, sum, diff;
 
+	srand(time(0));
+
+	for (i = 0, sum = 0; sum < 2772 - 122; i++)
+	{
+		password[i] = rand() % 94 + 33;
+		sum += password[i];
+	}
+
+	diff = sum - 2772 + 122;
+	password[i - 1] -= diff;
+
+	password[i] = '\0';
+
+	printf("%s", password);
+
+	return (0);
+}
