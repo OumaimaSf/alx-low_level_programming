f74b8784f630c4c35660891f0549f233692c7496
OumaimaSf <oumaimasaaouf@gmail.com>
2023-05-03 20:24:31 +0100

    atoi.c task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,30 +7,30 @@
  */
 int _atoi(char *s)
 {
-    int i, sign, n, len;
+	int i, sign, n, len;
 
-    i = 0;
-    sign = 1;
-    n = 0;
-    len = 0;
+	i = 0;
+	sign = 1;
+	n = 0;
+	len = 0;
 
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+		len++;
 
-    while (i < len && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	while (i < len && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    while (i < len && (s[i] >= '0' && s[i] <= '9'))
-    {
-        if (n > INT_MAX / 10 || (n == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-            return (sign == -1 ? INT_MIN : INT_MAX);
-        n = n * 10 + (s[i] - '0');
-        i++;
-    }
+	while (i < len && (s[i] >= '0' && s[i] <= '9'))
+	{
+		if (n > INT_MAX / 10 || (n == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+			return (sign == -1 ? INT_MIN : INT_MAX);
+		n = n * 10 + (s[i] - '0');
+		i++;
+	}
 
-    return (n * sign);
+	return (n * sign);
 }
