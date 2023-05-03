4b2fed79bc1c871463b774a4e454c47484e1980a
OumaimaSf <oumaimasaaouf@gmail.com>
2023-05-03 19:52:37 +0100

    atoi.c correction2

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,20 +7,25 @@
  */
 
 int _atoi(char *s)
+
 {
-	int sign = 1;
-	int num = 0;
-	int i = 0;
+	int i, n, sign;
+	
+	i = n = sign = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
-			num = num * 10 + (s[i] - '0');
-		if (num > 0 && (s[i] < '0' || s[i] > '9'))
+			sign = (sign == 0) ? -1 : sign;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (n > INT_MAX / 10 || (n == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+				return (sign == -1 ? INT_MIN : INT_MAX);
+			n = n * 10 + (s[i] - '0');
+		}
+		else if (n != 0)
 			break;
 		i++;
 	}
-	return (num * sign);
+	return (sign == -1 ? -n : n);
 }
