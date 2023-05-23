8a0577dad956594e9a6158f96d1d340a4b86c9d8
OumaimaSf <oumaimasaaouf@gmail.com>
2023-05-23 23:52:26 +0100

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * _strlen -returns the length of a string
+ * _strlen - returns the length of a string
  * @s: string to evaluate
  *
  * Return: the length of the string
@@ -20,6 +20,7 @@ int _strlen(char *s)
 
 	return (i);
 }
+
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
@@ -34,6 +35,7 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
+
 	while (src[len] != '\0')
 	{
 		len++;
@@ -51,10 +53,10 @@ char *_strcpy(char *dest, char *src)
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
- * @age: age of the dog 
+ * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to new dog (Success), NULL otherwise
+ * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
