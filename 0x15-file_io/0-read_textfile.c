e869070534f1136ed24a880a36d3a812045bd908
OumaimaSf <oumaimasaaouf@gmail.com>
2023-06-13 16:00:06 +0100

    Tread lightly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,6 +2,14 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * read_textfile - Read and print the content of a text file.
+ * @filename: Pointer to the name of the file to be read.
+ * @letters: Number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ *
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -12,11 +20,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t b_t_w;
 
 	if (filename == NULL)
-		return 0;
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return 0;
+		return (0);
 
 	while (letters > 0)
 	{
