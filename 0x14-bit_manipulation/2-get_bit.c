3743b43ccd8b4b1e69df4f6403fa4c624946a147
OumaimaSf <oumaimasaaouf@gmail.com>
2023-07-06 20:00:54 +0100

    Mandatory_Bit-Manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: The number to print in binary.
+ * @index: The index of the bit to retrieve.
+ *
+ * Return: The value of the bit at the given index, or -1 if an error.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int M;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	M = 1UL << index;
+	return (( n & M) ? 1 : 0);
+}
