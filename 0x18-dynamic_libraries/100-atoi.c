2c29baaa3460f39c653805196c65153fafcaabcd
OumaimaSf <oumaimasaaouf@gmail.com>
2023-12-20 21:54:13 +0100

    creat lib

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,36 @@
+#include <limits.h>
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer.
+ * @s: string to convert.
+ * Return: integer representation of string.
+ */
+int _atoi(char *s)
+{
+	int i, sign, n, len;
+
+	i = 0;
+	sign = 1;
+	n = 0;
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (i < len && (s[i] >= '0' && s[i] <= '9'))
+	{
+		if (n > INT_MAX / 10 || (n == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+			return (sign == -1 ? INT_MIN : INT_MAX);
+		n = n * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (n * sign);
+}
---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: The destination string
+ * @src: the source string
+ * @n: the maximum number of bytes to copy
+ *
+ * Return: A pointer to the destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
